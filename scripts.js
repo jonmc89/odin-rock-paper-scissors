1b83f4b0428ab30892c9fd7c3c735692865b972f
jonmc89 <126274870+jonmc89@users.noreply.github.com>
2025-01-04 21:38:29 +0000

    Setup Doc.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function getComputerChoice(){
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function handleRandomChoice(getComputerChoice){
+    let ComputerChoice = getComputerChoice;
+    switch{
+        
+    }
+}
